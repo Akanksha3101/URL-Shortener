64afe528b849cf0964c86832620a195a0922fd3f
Akanksha3101 <46569451+Akanksha3101@users.noreply.github.com>
2019-04-07 22:14:52 +0530

    Update validators.py

--- shortener/validators.py
+++ shortener/validators.py
@@ -1,28 +1,22 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
-from django.core.validators import ValidationError
 
 
 def validate_url(value):
-        url_validator = URLValidator()
-        value_1_invalid = False
-        value_2_invalid = False
-
-        try:
-            url_validator(value)
-        except:
-            value_1_invalid = True
-        value_2_url = "http://" + value
-        try:
-            url_validator(value_2_url)
-        except:
-            value_2_invalid = True
-
-        if value_1_invalid == False and value_2_invalid == False:
-            raise ValidationError("Invalid URL for this field")
-        return value
+    url_validator = URLValidator()
+    reg_val = value
+    if "http" in reg_val:
+        new_value = reg_val
+    else:
+        new_value = 'http://' + value
+    try:
+        url_validator(new_value)
+    except:
+        raise ValidationError("Invalid URL for this field")
+    return new_value
 
 
 def validate_dot_com(value):
-        if "com" not in value:
-            raise ValidationError("This is not valid because there is no .com")
-        return value
+    if not "com" in value:
+        raise ValidationError("This is not valid because of no .com")
+    return value
